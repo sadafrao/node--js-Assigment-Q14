96dc0d2c71f27cff5080dd9d13222a743d32d529
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:39:14 -0800

    Add files via upload

---
+++ Q14.js
@@ -0,0 +1,5 @@
+"use strict";
+//Q14. Guest List : If You Could Inivite anyone , Living or deceased , to dinner , who would you invite ? Make a list at least three people you'd like to 
+//invite to dinner then use your list to print message to each person , inviting them to dinner.
+let guestArr = ["Raza", "Sohail", "Fatima", "ume hani"];
+guestArr.map((item) => (console.log(`Dear ${item}, You Are Invited To a Dinner`)));
---
+++ Q14.ts
@@ -0,0 +1,7 @@
+//Q14. Guest List : If You Could Inivite anyone , Living or deceased , to dinner , who would you invite ? Make a list at least three people you'd like to 
+//invite to dinner then use your list to print message to each person , inviting them to dinner.
+
+let guestArr: string[] = ["Raza" ,"Sohail","Fatima","ume hani"]
+guestArr.map((item) => (
+    console.log(`Dear ${item}, You Are Invited To a Dinner`)
+))
